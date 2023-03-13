c37f703b0c52ff37f89757f7539123425e1a8bb1
Agyemang-SE <kevin.agyemang@proton.me>
2023-03-13 21:42:38 +0000

    Correction

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return 'None'
+    if len(my_list) - 1 < idx:
+        return 'None'
+    return(my_list[idx])
