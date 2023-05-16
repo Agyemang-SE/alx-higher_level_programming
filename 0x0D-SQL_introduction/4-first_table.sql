166e4f897c28cab38a1772696411f44c4ef0b392
Agyemang-SE <kevin.agyemang@proton.me>
2023-05-16 23:46:08 +0000

    Advanced Task completion

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
--- Script that creates a table called first_table in the current database in your MYSQL server
-CREATE TABLE IF NOT EXIST first_table (id INT, name VARCHAR(256));
+-- Script that creates a table called first_table in the current database in your MySQL server
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
 
