161bedb2bcda1f5d5e4cc84d54c24f1a53ec34d3
Agyemang-SE <root@localhost.localdomain>
2023-06-13 01:46:51 +0000

    Corrections to code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,6 @@
 const len = process.argv.length - 2;
 const print = console.log;
 
-if (!len) print('No arguments');
+if (!len) print('No argument');
 if (len === 1) print('Argument found');
-if (len > 2) print('Argument found');
+if (len > 1) print('Arguments found');
